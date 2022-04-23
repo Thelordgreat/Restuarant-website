b08901ba33d280e18c8b31bb01401f03cdd56c69
Pius Osuji <piusosuji9@gmail.com>
2022-04-24 08:25:11 +1200

    Navbar_Pius

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,30 @@
-import React from 'react'
-import NavBar from './Components/NavBar/NavBar'
-import Home from './Components/Home/Home'
-import About from './Components/About/About'
-import ProductsMenu from './Components/ProductsMenu/ProductsMenu'
-import Testimonials from './Components/Testimonials/Testimonials'
-import Contact from './Components/Contact/Contact'
-import Footer from './Components/Footer/Footer'
+import React from "react";
+import NavBar from "./Components/NavBar/NavBar";
+import Home from "./Components/Home/Home";
+import About from "./Components/About/About";
+import ProductsMenu from "./Components/ProductsMenu/ProductsMenu";
+import Testimonials from "./Components/Testimonials/Testimonials";
+import Contact from "./Components/Contact/Contact";
+import Footer from "./Components/Footer/Footer";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
 
 const App = () => {
   return (
-
-
     <>
       <Router>
-      <NavBar/>
+        <NavBar />
 
         <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/menu' element={<ProductsMenu/>}/>
-          <Route path='/testimonial' element={<Testimonials/>}/>
-          <Route path='/contact' element={<Contact/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/menu" element={<ProductsMenu />} />
+          <Route path="/testimonial" element={<Testimonials />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
-      <Footer/>
+        <Footer />
       </Router>
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
