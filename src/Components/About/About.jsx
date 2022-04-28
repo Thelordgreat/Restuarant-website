73c23582d6ac776f5b236e14db7e569f99c43df1
Pius Osuji <piusosuji9@gmail.com>
2022-04-28 19:51:10 +1200

    Products and footer

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,57 +1,87 @@
-import React from 'react'
-import './About.css'
-import { MdFastfood }from 'react-icons/md'
-import { GiMeal } from 'react-icons/gi'
-import { HiCake } from 'react-icons/hi'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "./About.css";
+import { MdFastfood } from "react-icons/md";
+import { GiMeal } from "react-icons/gi";
+import { HiCake } from "react-icons/hi";
+import { Link } from "react-router-dom";
 
 const About = () => {
   return (
     <section>
       <div className="about__container">
-          <div className="about__img">
-            <div className="about__img__text">
-              <h2>Customer Satisfaction Is Our Main Priority</h2>
-                <Link to={'./products'}><a href="google.com" className='btn'>View Menu</a></Link>
-            </div>
+        <div className="about__img">
+          <div className="about__img__text">
+            <h2>Customer Satisfaction Is Our Main Priority</h2>
+            <Link to={"./products"}>
+              <a href="google.com" className="btn">
+                View Menu
+              </a>
+            </Link>
+          </div>
+        </div>
+
+        <div className="about__body__content">
+          <div className="about__body__text">
+            <h2>About Us</h2>
+
+            <p>
+              Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+              <br /> Soluta totam assumenda qui est pariatur earum eum sit,
+              <br /> amet culpa autem recusandae nulla quod
+              <br /> magni omnis minus molestias ab molestiae magnam.
+            </p>
           </div>
 
-          <div className="about__body__content">
-            <div className="about__body__text">
-              <h2>About Us</h2>
+          <div className="about__boxes__container">
+            <div className="box">
+              <MdFastfood className="icon" />
+              <h3>Food</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                tempora laboriosam iste exercitationem non
+              </p>
 
-              <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta totam assumenda qui est pariatur earum eum sit, amet culpa autem recusandae nulla quod magni omnis minus molestias ab molestiae magnam.</p>
+              <Link to={"./products"}>
+                <a href="google.com" className="btn">
+                  View Service
+                </a>
+              </Link>
             </div>
 
-            <div className="about__boxes__container">
-              <div className="box">
-                  <MdFastfood className='icon'/>
-                  <h3>Food</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum tempora laboriosam iste exercitationem non</p>
+            <div className="box">
+              <GiMeal className="icon" />
+              <h3>Dishes</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                tempora laboriosam iste exercitationem non
+              </p>
 
-                  <Link to={'./products'}><a href="google.com" className='btn'>View Service</a></Link>
-              </div>
+              <Link to={"./products"}>
+                <a href="google.com" className="btn">
+                  View Service
+                </a>
+              </Link>
+            </div>
 
-              <div className="box">
-                  <GiMeal className='icon'/>
-                  <h3>Dishes</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum tempora laboriosam iste exercitationem non</p>
+            <div className="box">
+              <HiCake className="icon" />
+              <h3>Birthdays</h3>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum
+                tempora laboriosam iste exercitationem non
+              </p>
 
-                  <Link to={'./products'}><a href="google.com" className='btn'>View Service</a></Link>
-              </div>
-
-              <div className="box">
-                  <HiCake className='icon'/>
-                  <h3>Birthdays</h3>
-                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum tempora laboriosam iste exercitationem non</p>
-
-                  <Link to={'./products'}><a href="google.com" className='btn'>View Service</a></Link>
-              </div>
+              <Link to={"./products"}>
+                <a href="google.com" className="btn">
+                  View Service
+                </a>
+              </Link>
             </div>
           </div>
+        </div>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default About
+export default About;
--- src/Components/ProductsMenu/ProductsMenu.jsx
+++ src/Components/ProductsMenu/ProductsMenu.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
-import './ProductsMenu.css'
+import React from "react";
+import "./ProductsMenu.css";
+
+import food1 from "../../images/food1.jpg";
+import food2 from "../../images/food2.jpg";
+import food3 from "../../images/food3.jpg";
+import food4 from "../../images/food4.jpg";
+import food5 from "../../images/food5.jpg";
+import food6 from "../../images/food6.jpg";
+import food7 from "../../images/food7.jpg";
+import food8 from "../../images/food8.jpg";
+import food9 from "../../images/food9.jpg";
+
 const ProductsMenu = () => {
   return (
-    <div>ProductsMenu</div>
-  )
-}
+    <div className="products">
+      <h2>Our Menu</h2>
+      <div className="dishes">
+        <img src={food1} alt="" />
+        <img src={food2} alt="" />
+        <img src={food3} alt="" />
+        <img src={food4} alt="" />
+        <img src={food5} alt="" />
+        <img src={food6} alt="" />
+        <img src={food7} alt="" />
+        <img src={food8} alt="" />
+        <img src={food9} alt="" />
+      </div>
+    </div>
+  );
+};
 
-export default ProductsMenu
+export default ProductsMenu;
